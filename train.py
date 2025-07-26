9389cda23c7fb1eae98b30f759bd4479071b4171
agupta451 <167340941+agupta451@users.noreply.github.com>
2025-07-26 22:28:06 +0530

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,8 @@
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 import joblib
-
+import os
+os.makedirs("app", exist_ok=True)
 data = load_iris()
 X, y = data.data, data.target
 
